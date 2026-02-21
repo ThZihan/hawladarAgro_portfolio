5ec53623eb5f31af099fcd6d4a45d7be9d300fc1
Tahfizul Hasan Zihan <tahfijulhassanjehan@gmail.com>
2026-02-22 00:25:10 +0600

    Fix static files serving in development

--- hawladar_agro/urls.py
+++ hawladar_agro/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
 
 # Add static and media file serving in development
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
